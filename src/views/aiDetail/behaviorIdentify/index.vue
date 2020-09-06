4fc80354eb07136ffc82124f19c0a5ee52543e72
lovepotato <659362770@qq.com>
2020-09-07 00:29:15 +0800

    人工智能详情页面初始化

--- src/views/aiDetail/behaviorIdentify/index.vue
+++ src/views/aiDetail/behaviorIdentify/index.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>行为识别</div>
+  <div class="behaviorIdentify">
+    <div class="left-contaier"></div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'BehaviorIdentify',
+  data() {
+    return {
+      baseData: {}
+    }
+  },
+  created() {
+    this.getDetail()
+  },
+  methods: {
+    getDetail() {
+      this.http.post(`/cloudvideo/index`).then(res => {
+        this.baseData = res
+      })
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.behaviorIdentify {
+  width: 100%;
+  height: 1000px;
+  display: flex;
+}
 
+.left-contaier {
+  background-image: url("../../../assets/imgs/行为识别Group1.png");
+  width: 949px;
+  height: 994px;
+  background-repeat: no-repeat;
+  background-size: 100%;
+}
 </style>
--- src/views/aiDetail/index.vue
+++ src/views/aiDetail/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <el-tabs v-model="activeName" type="card" @tab-click="handClick">
+  <div class="ai-detail">
+    <el-tabs v-model="activeName" type="card" @tab-click="handClick" class="tab">
       <el-tab-pane label="行为识别" name="behaviorIdentify"></el-tab-pane>
       <el-tab-pane label="健康监测" name="healthMonitor"></el-tab-pane>
       <el-tab-pane label="康复管理" name="rehabilitation"></el-tab-pane>
@@ -28,6 +28,65 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.ai-detail {
+  padding: 20px 55px 55px 55px;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  border: 1px solid red;
 
+  .tab {
+    margin-bottom: 20px;
+  }
+
+  .el-tabs__header {
+    margin: 0px !important;
+  }
+  .el-tabs__nav-prev, .el-tabs__nav-next {
+    display: none !important;
+  }
+
+  .el-tabs--card>.el-tabs__header {
+    border-bottom: 0px !important;
+  }
+
+  .el-tabs--card>.el-tabs__header .el-tabs__nav {
+    border: 0px !important;
+  }
+
+  .el-tabs--card>.el-tabs__header .el-tabs__item {
+    border-bottom: 0px !important;
+    border-left: 0px !important;
+  }
+
+  .el-tabs__item {
+    width: 384px;
+    height: 69px;
+    background-color: transparent;
+    font-family: PingFangSC-Regular;
+    font-size: 26px;
+    color: #0091FF;
+    text-align: center;
+    line-height: 69px;
+    margin-right: 208px;
+    padding: 0px;
+
+    &:last-child {
+      margin-right: 0px;
+    }
+  }
+
+  .el-tabs__item.is-active {
+    width: 384px;
+    height: 69px;
+    background-color: #032F8C;
+    color: #fff;
+    font-family: PingFangSC-Semibold;
+    font-size: 26px;
+    border-radius: 8px 8px 0px 0px;
+    text-align: center;
+    line-height: 69px;
+  }
+}
 </style>
