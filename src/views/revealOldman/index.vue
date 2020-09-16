1acc4822e6e8edc47130b5d56ed748057aa8b191
limeng <594968121@qq.com>
2020-09-17 02:05:49 +0800

    兜底老人初步编写

--- src/views/revealOldman/index.vue
+++ src/views/revealOldman/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="revealOldman-container">
-    <div class="revealOldman-warper">
+    <div class="revealOldman-map-view">
       <div class="left-info">
         <div class="flowsheet-content"></div>
         <div class="safe-record-list"></div>
@@ -12,22 +12,48 @@
         <div class="oldman-count-list"></div>
         <div class="care-info-list"></div>
       </div>
-      <div class="all-oldman-list"></div>
+      <div class="all-oldman-list">
+        <oldman-info-box v-for="i in 6" :key="i"></oldman-info-box>
+      </div>
     </div>
+    <div class="revealOldman-box-view">
+
+    </div>
+    <div class="fixed-banner">
+      <div class="banner-title">今日动态</div>
+      <div class="banner-item" v-for="i in 4" :key="i">
+        <div class="value"></div>
+        <div class="title"></div>
+      </div>
+    </div>
+    <div class="toggle-view-btn" :class="{'diplay-all' : isDisplayAll}" @click="toggleDisplayAllBox"></div>
   </div>
 </template>
 <script>
 import revealOldmanMap from './revealOldmanMap/revealOldmanMap'
+import oldmanInfoBox from './oldmanInfoBox/oldmanInfoBox'
 export default {
   name: 'RevealOldman',
-  components: { revealOldmanMap }
+  components: { revealOldmanMap, oldmanInfoBox },
+  data() {
+    return {
+      isDisplayAll: false
+    }
+  },
+  methods: {
+    // 切换显示模式
+    toggleDisplayAllBox() {
+      this.isDisplayAll = !this.isDisplayAll
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
 .revealOldman-container{
   width: 100%;
   height: 100%;
-  .revealOldman-warper{
+  position: relative;
+  .revealOldman-map-view{
     margin-top: 23px;
     height: calc(100% - 23px);
     width: 100%;
@@ -81,7 +107,65 @@ export default {
       }
     }
     .all-oldman-list{
-      position: absolute;
+      width: 866px;
+      height: 100%;
+      // margin-right:35px;
+      display: flex;
+      flex-wrap: wrap;
+    }
+  }
+  .revealOldman-box-view{
+    margin-top: 23px;
+    height: calc(100% - 23px);
+    width: 100%;
+  }
+  .fixed-banner{
+    height: 1079px;
+    width: 146px;
+    padding-left: 166px;
+    position: absolute;
+    right: 119px;
+    top: 0;
+    background-color: #032F8C;
+    padding: 20px 10px;
+    .banner-title{
+      height: 56px;
+      width: 125px;
+      line-height: 56px;
+      font-size: 20px;
+      color: #FFFFFF;
+      letter-spacing: 4.3px;
+      text-align: center;
+      background-image: url('../../assets/imgs/今日动态框.png');
+    }
+    .banner-item{
+      width: 100%;
+      height: 244px;
+      position: relative;
+      &::after{
+        content: '';
+        width: 100%;
+        height: 2px;
+        background-color: #32C5FF;
+        position: absolute;
+        bottom: 0;
+      }
+      &:last-child::after{
+        display: none;
+      }
+    }
+  }
+  .toggle-view-btn{
+    position: absolute;
+    width: 51px;
+    height: 51px;
+    right: 55px;
+    background-image: url('../../assets/imgs/箭头》.png');
+    top: 50%;
+    transform: translateY(-50%);
+    cursor: pointer;
+    &.diplay-all{
+      background-image: url('../../assets/imgs/箭头《.png');
     }
   }
 }
---
+++ src/views/revealOldman/oldmanInfoBox/oldmanInfoBox.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="oldman-list-item">
+    <div class="safe-status" :class="[oldmanDetail.safeStatus === 0 ? 'is-sure-safe' : 'is-unsure-safe']">{{ oldmanDetail.safeStatus == 0 ? '已报平安' : '未报平安' }}</div>
+    <div class="box-header">
+      <div class="box-img">
+        <el-avatar :size="99" :src="oldmanDetail.img">
+        </el-avatar>
+      </div>
+      <div class="oldman-base-info">
+        <div class="info-top info-item">{{ oldmanDetail.name }}</div>
+        <div class="info-center info-item">
+          <div class="tag-item" v-for="(tag, index) in oldmanDetail.tags" :key="index">{{ tag }}</div>
+        </div>
+        <div class="info-bottom info-item">
+          <span class="gender">{{ oldmanDetail.gender }}</span>
+          <span class="age">{{ oldmanDetail.age | fliterAge }}</span>
+        </div>
+      </div>
+    </div>
+    <div class="contract-info">
+      <div class="phone-info contract-info-item">
+        <div class="img-icon phone-img"></div>
+        <div class="info-title">联系电话</div>
+        <div class="info-value">{{ oldmanDetail.phone }}</div>
+      </div>
+      <div class="address-info contract-info-item">
+        <div class="img-icon address-img"></div>
+        <div class="info-title">详细地址</div>
+        <div class="info-value">{{ oldmanDetail.address }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RevealOldmanMap',
+  filters: {
+    fliterAge(age) {
+      return age + '岁'
+    }
+  },
+  props: {
+    oldmanDetail: {
+      type: Object,
+      default() {
+        return {
+          name: '张奶奶',
+          gender: '女',
+          age: '79',
+          address: '大兴区国际港1区2号楼3单元302',
+          safeStatus: 0,
+          tags: ['高龄', '空巢']
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.oldman-list-item{
+  width: 398px;
+  height: 327px;
+  padding: 34px 32px;
+  background-image: url('../../../assets/imgs/兜底老人框3.png');
+  margin-right: 35px;
+  margin-bottom: 49px;
+  position: relative;
+  .safe-status{
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 110px;
+    height: 40px;
+    font-size: 20px;
+    color: #F9F9F9;
+    letter-spacing: 0;
+    line-height: 40px;
+    text-align: center;
+    &.is-sure-safe{
+      background-image: url('../../../assets/imgs/标签蓝.png');
+    }
+    &.is-unsure-safe{
+      background-image: url('../../../assets/imgs/标签红.png');
+    }
+  }
+  .box-header{
+    height: 99px;
+    width: 100%;
+    display: flex;
+    .box-img{
+      width: 99px;
+      height: 99px;
+      margin-right: 19px;
+    }
+    .oldman-base-info{
+      width: calc(100% - 99px - 19px);
+      height: 99px;
+      .info-item{
+        height: 30px;
+        line-height: 30px;
+        font-size: 22px;
+        color: #FFFFFF;
+        letter-spacing: 0;
+        .age{
+          padding-left: 20px;
+        }
+        .tag-item{
+          border: 1px solid #32C5FF;
+          border-radius: 4px;
+          font-size: 16px;
+          color: #32C5FF;
+          letter-spacing: 0;
+          padding: 0 9px;
+          height: 24px;
+          line-height: 24px;
+          margin-right: 10px;
+        }
+        // &.info-top{
+        //   margin-bottom: 41px;
+        // }
+      }
+      .info-center{
+        display: flex;
+        justify-self: start;
+        margin: 10px 0;
+        height: 24px;
+      }
+    }
+  }
+  .contract-info{
+    height: 107px;
+    width: 100%;
+    margin-top: 56px;
+    .contract-info-item{
+      display: flex;
+      .img-icon{
+        width: 16px;
+        height: 16px;
+        margin-top: 7px;
+        &.phone-img{
+          background-image: url('../../../assets/imgs/dianhua.png');
+        }
+        &.address-img{
+          background-image: url('../../../assets/imgs/dizhi.png');
+        }
+      }
+      .info-title{
+        width: 72px;
+        height: 30px;
+        font-size: 18px;
+        color: #32C5FF;
+        letter-spacing: 0;
+        line-height: 30px;
+        margin-left: 11px;
+        margin-right: 17px;
+      }
+      .info-value{
+        height: 30px;
+        font-size: 18px;
+        color: #FFFFFF;
+        letter-spacing: 0;
+        line-height: 30px;
+        width: 209px;
+      }
+    }
+    .phone-info{
+      height: 30px;
+      font-size: 18px;
+      letter-spacing: 0;
+      line-height: 30px;
+      margin-bottom: 17px;
+    }
+    .address-info{
+      height: 60px;
+      .info-value{
+        height: 60px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+      }
+    }
+  }
+}
+</style>
