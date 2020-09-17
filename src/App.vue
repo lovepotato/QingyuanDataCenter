7f2b1141060c41087843e13b2e8dd483bbf782a6
lovepotato <659362770@qq.com>
2020-09-17 22:36:19 +0800

    1.66比例自动拉伸

--- src/App.vue
+++ src/App.vue
@@ -2,101 +2,81 @@
   <div id="app">
     <!-- <vue-scroll> -->
     <router-view />
-    <aged-detail></aged-detail>
-    <number-modal></number-modal>
-    <sos-modal></sos-modal>
-    <warning-modal></warning-modal>
     <!-- </vue-scroll> -->
   </div>
 </template>
 
 <script>
-import agedDetail from './components/Modal/agedDetail/index'
-import numberModal from './components/Modal/Number'
-import sosModal from './components/Modal/SosModal'
-import warningModal from './components/Modal/WarningModal'
 
 export default {
   name: 'App',
-  components: {
-    agedDetail,
-    numberModal,
-    sosModal,
-    warningModal
-  },
-  data() {
-    return {
-      setIntervalMessage: null,
-      setIntervalZhixiaoyun: null
-    }
-  },
   mounted() {
     // this.setScale()
     // window.addEventListener('resize', this.setScale)
-    this.loopMessage()
+    // this.loopMessage()
     // this.loopZhixiaoyun()
   },
   destroyed() {
-    if (this.setIntervalMessage) {
-      clearInterval(this.setIntervalMessage)
-    }
+    // if (this.setIntervalMessage) {
+    //   clearInterval(this.setIntervalMessage)
+    // }
 
-    if (this.setIntervalZhixiaoyun) {
-      clearInterval(this.setIntervalZhixiaoyun)
-    }
+    // if (this.setIntervalZhixiaoyun) {
+    //   clearInterval(this.setIntervalZhixiaoyun)
+    // }
 
-    window.removeEventListener('resize', this.setScale)
+    // window.removeEventListener('resize', this.setScale)
   },
   methods: {
-    loopMessage() {
-      this.setIntervalMessage = setInterval(this.timerRequestMessage(), 20000)
-    },
-    timerRequestMessage() {
-      this.http.post('/commandcenter/message/notify').then(({ code, data }) => {
-        if (code === 0) {
-          this.$bus.$emit('closeModal')
-          if (Number(data.type) === 2) {
-            this.$bus.$emit('newMobile', { data: data.data })
-          }
+    // loopMessage() {
+    //   this.setIntervalMessage = setInterval(this.timerRequestMessage(), 20000)
+    // },
+    // timerRequestMessage() {
+    //   this.http.post('/commandcenter/message/notify').then(({ code, data }) => {
+    //     if (code === 0) {
+    //       this.$bus.$emit('closeModal')
+    //       if (Number(data.type) === 2) {
+    //         this.$bus.$emit('newMobile', { data: data.data })
+    //       }
 
-          if (Number(data.type) === 1) {
-            this.$bus.$emit('newSosModal', { data: data.data })
-          }
+    //       if (Number(data.type) === 1) {
+    //         this.$bus.$emit('newSosModal', { data: data.data })
+    //       }
 
-          if (Number(data.type) === 3) {
-            this.$bus.$emit('newWarningModal', { data: data.data })
-          }
-        } else {
-          return
-        }
-      })
-      return this.timerRequestMessage
-    },
-    loopZhixiaoyun() {
-      this.setIntervalZhixiaoyun = setInterval(() => {
-        this.http.post('/smartcloud/oldman_search', { keyword: '1' }).then((res) => {
-          console.log(res)
-        })
-      }, 2000)
-    },
-    getScale() {
-      const width = 3456
-      const height = 1296
-      const ww = window.innerWidth / width
-      const wh = window.innerHeight / height
-      return ww < wh ? ww : wh
-    },
-    setScale() {
-      this.scale = this.getScale()
-      document.body.style.setProperty('--scale', this.scale)
-    }
+    //       if (Number(data.type) === 3) {
+    //         this.$bus.$emit('newWarningModal', { data: data.data })
+    //       }
+    //     } else {
+    //       return
+    //     }
+    //   })
+    //   return this.timerRequestMessage
+    // },
+    // loopZhixiaoyun() {
+    //   this.setIntervalZhixiaoyun = setInterval(() => {
+    //     this.http.post('/smartcloud/oldman_search', { keyword: '1' }).then((res) => {
+    //       console.log(res)
+    //     })
+    //   }, 2000)
+    // },
+    // getScale() {
+    //   const width = 3456
+    //   const height = 1296
+    //   const ww = window.innerWidth / width
+    //   const wh = window.innerHeight / height
+    //   return ww < wh ? ww : wh
+    // },
+    // setScale() {
+    //   this.scale = this.getScale()
+    //   document.body.style.setProperty('--scale', this.scale)
+    // }
   }
 }
 </script>
 
 <style>
-body {
+/* body {
   --scale: 1;
-  /* transform: scale(var(--scale), 0.833) ; */
-}
+  transform: scale(var(--scale), 0.833) ;
+} */
 </style>
